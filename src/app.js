6aa36fa37cc4c9d1986084c111f95227764ed47a
Kindikeri Vyjayanthi Reddy <56124953+vyjayanthi22@users.noreply.github.com>
2021-07-13 03:18:25 +0530

    Add files via upload

---
+++ src/app.js
@@ -0,0 +1,20 @@
+let express = require('express');
+let app = express();
+let server = require('http').Server(app);
+let io = require('socket.io')(server);
+let stream = require('./ws/stream');
+let path = require('path');
+const port = process.env.port||3000;
+app.use('/assets', express.static(path.join(__dirname, 'assets')));
+
+app.get('/', (req, res)=>{
+    res.sendFile(__dirname+'/index.html');
+});
+
+
+io.of('/stream').on('connection', stream);
+
+// server.listen(3000);
+server.listen(port, () => {
+    console.log('Server running at '+port);
+  });
